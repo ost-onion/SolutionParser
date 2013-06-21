5aacf72e167fa3c9a03661ec52429a830f1ddaa7
Brian Scaturro <scaturrob@gmail.com>
2013-06-21 12:26:04 -0400

    add GlobalSectionParser and test fixtures

---
+++ src/Onion.SolutionParser.Parser/GlobalSectionParser.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Onion.SolutionParser.Parser.Model;
+
+namespace Onion.SolutionParser.Parser
+{
+    public class GlobalSectionParser
+    {
+        private readonly string _solutionContents;
+        private static readonly Regex GlobalPattern = new Regex(@"GlobalSection\((?<name>[\w]+)\)\s+=\s+(?<type>(?:post|pre)Solution)(?<content>.*?)EndGlobalSection", RegexOptions.Singleline | RegexOptions.ExplicitCapture);
+        private static readonly Regex EntryPattern = new Regex(@"^\s+(?<key>.*?)=(?<value>.*?)$", RegexOptions.ExplicitCapture | RegexOptions.Multiline);
+
+        public GlobalSectionParser(string solutionContents)
+        {
+            _solutionContents = solutionContents;
+        }
+
+        public IEnumerable<GlobalSection> Parse()
+        {
+            var match = GlobalPattern.Match(_solutionContents);
+            while (match.Success)
+            {
+                var section = CreateGlobalSectionFromMatch(match);
+                PopulateSectionEntries(match, section);
+                yield return section;
+                match = match.NextMatch();
+            }
+        }
+
+        private static void PopulateSectionEntries(Match match, GlobalSection section)
+        {
+            var content = match.Groups["content"].Value;
+            var entryMatch = EntryPattern.Match(content);
+            while (entryMatch.Success)
+            {
+                var key = entryMatch.Groups["key"].Value.Trim();
+                var value = entryMatch.Groups["value"].Value.Trim();
+                section.Entries.Add(key, value);
+                entryMatch = entryMatch.NextMatch();
+            }
+        }
+
+        private static GlobalSection CreateGlobalSectionFromMatch(Match match)
+        {
+            var sectionType = (match.Groups["type"].Value == "preSolution")
+                                  ? GlobalSectionType.PreSolution
+                                  : GlobalSectionType.PostSolution;
+            var section = new GlobalSection(match.Groups["name"].Value, sectionType);
+            return section;
+        }
+    }
+}
---
+++ tests/Onion.SolutionParser.Tests/Parser/GlobalSectionParserTest.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Onion.SolutionParser.Parser;
+using Onion.SolutionParser.Parser.Model;
+
+namespace Onion.SolutionParser.Tests.Parser
+{
+    [TestFixture]
+    public class GlobalSectionParserTest
+    {
+        public GlobalSectionParser Parser { get; set; }
+        public string SolutionContents { get; set; }
+
+        [TestFixtureSetUp]
+        public void BeforeAll()
+        {
+            var fixturesPath = Path.GetFullPath("Fixtures");
+            var slnPath = fixturesPath + Path.DirectorySeparatorChar + "NDriven.sln";
+            using (var reader = new StreamReader(slnPath))
+            {
+                SolutionContents = reader.ReadToEnd();
+            }
+        }
+
+        [SetUp]
+        public void BeforeEach()
+        {
+            Parser = new GlobalSectionParser(SolutionContents);
+        }
+
+        [Test]
+        public void Parse_should_return_IEnumerable_of_GlobalSection_objects_correct_count()
+        {
+            var sections = Parser.Parse();
+            var count = sections.Count();
+            Assert.IsInstanceOf<IEnumerable<GlobalSection>>(sections);
+            Assert.AreEqual(4, count);
+        }
+
+        [Test]
+        public void Parse_should_hydrate_GlobalSection_object_name_and_type_properties()
+        {
+            var sections = Parser.Parse();
+            foreach(var section in sections)
+            {
+                Assert.IsNotEmpty(section.Name);
+                Assert.IsInstanceOf<GlobalSectionType>(section.Type);
+            }
+        }
+
+        [Test]
+        public void Parse_should_hydrate_entries()
+        {
+            var sections = Parser.Parse();
+            foreach (var section in sections)
+            {
+                Assert.True(section.Entries.Count > 0);
+            }
+        }
+
+        [Test]
+        public void GlobalSections_should_have_correct_number_of_entries()
+        {
+            var sections = Parser.Parse();
+            Assert.AreEqual(2, sections.ElementAt(0).Entries.Count);
+            Assert.AreEqual(32, sections.ElementAt(1).Entries.Count);
+            Assert.AreEqual(1, sections.ElementAt(2).Entries.Count);
+            Assert.AreEqual(6, sections.ElementAt(3).Entries.Count);
+        }
+    }
+}
