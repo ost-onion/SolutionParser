f124b3a3e06c0dd9429a961cf95ba14c073858f7
Brian Scaturro <scaturrob@gmail.com>
2013-06-23 15:54:02 -0400

    add solution model

---
+++ src/Onion.SolutionParser.Parser/Model/ISolution.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Onion.SolutionParser.Parser.Model
+{
+    public interface ISolution
+    {
+        IEnumerable<GlobalSection> Global { get; }
+        IEnumerable<Project> Projects { get; }
+    }
+}
---
+++ src/Onion.SolutionParser.Parser/Model/Solution.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Onion.SolutionParser.Parser.Model
+{
+    public class Solution : ISolution
+    {
+        public IEnumerable<GlobalSection> Global { get; set; }
+        public IEnumerable<Project> Projects { get; set; } 
+    }
+}
