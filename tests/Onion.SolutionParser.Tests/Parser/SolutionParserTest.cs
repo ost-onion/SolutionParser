2d99fe14d3246527ba309f42473243d8c2d6a80b
Brian Scaturro <scaturrob@gmail.com>
2013-06-24 08:12:16 -0400

    solution parser test

---
+++ tests/Onion.SolutionParser.Tests/Parser/SolutionParserTest.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using NUnit.Framework;
+using RealParser = Onion.SolutionParser.Parser;
+
+namespace Onion.SolutionParser.Tests.Parser
+{
+    [TestFixture]
+    public class SolutionParserTest
+    {
+        [Test]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void Constructing_with_unknown_path_throws_exception()
+        {
+            var parser = new RealParser.SolutionParser("nope");
+        }
+    }
+}
